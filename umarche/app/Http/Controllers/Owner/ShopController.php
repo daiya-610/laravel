76b723b56133d35a1e44c17a3f970f0f1a666ec5
daiya-610 <daiya06010@gmail.com>
2024-07-03 00:43:18 +0900

    shop editの残り　画像アップロード、ブラウザに反映

--- umarche/app/Http/Controllers/Owner/ShopController.php
+++ umarche/app/Http/Controllers/Owner/ShopController.php
@@ -50,6 +50,11 @@ class ShopController extends Controller
 
     public function update(UploadImageRequest $request, string $id)
     {
+        $request->validate([
+            'name' => 'required|string|max:50',
+            'information' => 'required|string|max:1000',
+            'is_selling' => 'required',
+        ]);
         // バリデーションが通った後の処理
         $imageFile = $request->image;
         if(!is_null($imageFile) && $imageFile->isValid() ){
@@ -61,6 +66,20 @@ class ShopController extends Controller
 
             // Storage::put('public/shops/' .$fileNameToStore, $imageFile );
         }
-        return redirect()->route('owner.shops.index')->with('success', '更新されました。');
+
+        $shop = Shop::findOrFail($id);
+        $shop->name = $request->name;
+        $shop->information = $request->information;
+        $shop->is_selling = $request->is_selling;
+        if(!is_null($imageFile) && $imageFile->isValid() ){
+            $shop->filename = $fileNameToStore;
+        }
+
+        $shop->save();
+
+        return redirect()
+        ->route('owner.shops.index')
+        ->with(['message' => '店舗情報を更新しました。',
+        'status' => 'info']);
     }
 }
